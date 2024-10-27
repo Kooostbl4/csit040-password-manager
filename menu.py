4c41c4c23570f97ca80e4a342797fa10d6b88385
Kooostbl4 <konstantin.i13@mail.ru>
2024-10-27 15:22:53 +0400

    first commit

---
+++ menu.py
@@ -0,0 +1,37 @@
+from users import register, login
+from passwords import *
+
+def start_menu(conn, status="logged_out"):
+    try:
+        print("-"*100)
+        print("Welcome to Password Manager!")
+        print("-"*100)
+
+        if status == "logged_in":
+            print("What do you want to do?\n")
+            print("1. Add new password")
+            print("2. Look up the password")
+            print("3. Delete password")
+            print("4. Change password")
+            print("5. Log out")
+            print("6. Exit") 
+
+            choice = input("Enter your choice: ")
+
+        elif status == "logged_out":
+            print("You are not logged in\n")
+            print("1. Log in")
+            print("2. Register")
+            print("3. Exit")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                login(conn)
+
+            elif choice == "2":
+                register(conn)
+
+    except:
+        print("An error occurred")
+        start_menu(conn, "logged_out")
